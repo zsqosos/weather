7acaac69283caf89203d1fa722b43c0f9ede560e
MEMEME <zsqos@hotmail.com>
2017-04-16 14:49:22 +0800

    updata change weather api

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 /**
- * Created by MEMEME on 2016/11/13.
+ * Created by zsq on 2016/11/13.
  */
+//调用jsonp函数请求当前所在城市
+jsonp('https://api.map.baidu.com/api?v=2.0&ak=Dv1NMU23dh1sGS9n2tUouDEYY96Dfzh3&s=1&callback=getCity');
 window.onload=function(){
-    //调用jsonp函数请求当前所在城市
-    jsonp('https://api.map.baidu.com/api?v=2.0&ak=Dv1NMU23dh1sGS9n2tUouDEYY96Dfzh3&s=1&callback=getCity');
     //请求天气车数据
     btn.onclick=function (){
-        jsonp(createUrl());
+        jsonp(createUrl()[0]);
+        jsonp(createUrl()[1]);
     }
 };
 
 function getCity(){
     function city(result){
-        jsonp(createUrl(result.name));
+        //去掉城市名后的"市"
+        var city = result.name.substring(0,result.name.length-1);
+        //请求当前城市的天气数据
+        jsonp(createUrl(city)[0]);
+        jsonp(createUrl(city)[1]);
     }
     var cityName = new BMap.LocalCity();
     cityName.get(city);
@@ -29,41 +34,46 @@ function jsonp(url){
 //数据请求成功回调函数，用于将获取到的数据放入页面相应位置
 function getWeather(response) {
     var oSpan = document.getElementsByClassName('info');
-    var data = response.result.data;
-    oSpan[0].innerHTML=data.realtime.city_name;
-    oSpan[1].innerHTML=data.realtime.date;
-    oSpan[2].innerHTML='星期'+data.weather[0].week;
-    oSpan[3].innerHTML=data.realtime.weather.info;
-    oSpan[4].innerHTML=data.realtime.weather.temperature+'℃';
-    oSpan[5].innerHTML=data.realtime.wind.direct;
-    oSpan[6].innerHTML=data.realtime.weather.humidity+'%';
-    oSpan[7].innerHTML=data.realtime.time;
-    oSpan[8].innerHTML=data.life.info.ziwaixian[0];
-    oSpan[9].innerHTML=data.life.info.xiche[0];
-    oSpan[10].innerHTML=data.life.info.kongtiao[0];
-    oSpan[11].innerHTML=data.life.info.chuanyi[0];
+    var data = response.result;
+    oSpan[0].innerHTML=data[0].citynm;
+    oSpan[1].innerHTML=data[0].days;
+    oSpan[2].innerHTML=data[0].week;
+    oSpan[3].innerHTML=data[0].weather;
+    oSpan[4].innerHTML=data[0].temperature;
+    oSpan[5].innerHTML=data[0].winp;
+    oSpan[6].innerHTML=data[0].wind;
 
     var aDiv = document.getElementsByClassName('future_box');
     for(var i=0; i<aDiv.length; i++){
         var aSpan = aDiv[i].getElementsByClassName('future_info');
-        aSpan[0].innerHTML = data.weather[i].date;
-        aSpan[1].innerHTML = '星期'+data.weather[i].week;
-        aSpan[2].innerHTML =data.weather[i].info.day[1];
-        aSpan[3].innerHTML = data.weather[i].info.day[2]+'℃';
+        aSpan[0].innerHTML = data[i+1].days;
+        aSpan[1].innerHTML = data[i+1].week;
+        aSpan[2].innerHTML =data[i+1].weather;
+        aSpan[3].innerHTML = data[i+1].temperature;
     }
-
+//根据返回数据，替换不同天气图片
     changeImg(response);
+}
+
+function getTodayWeather(response){
+    var oSpan = document.getElementsByClassName('info');
+    var data = response.results;
+    oSpan[7].innerHTML=data[0].pm25;
+    oSpan[8].innerHTML=data[0].index[4].zs;
+    oSpan[9].innerHTML=data[0].index[1].zs;
+    oSpan[10].innerHTML=data[0].index[2].zs;
+    oSpan[11].innerHTML=data[0].index[0].zs;
 }
 
 //根据获取到的数据更改页面中相应的图片
 function changeImg(data){
     var firstImg = document.getElementsByTagName("img")[0];
-    var firstWeatherId=data.result.data.realtime.weather.img;
+    var firstWeatherId=data.result[0].weatid;
     chooseImg(firstWeatherId,firstImg);
 
     var aImg = document.getElementById('future_container').getElementsByTagName('img');
     for(var j=0; j<aImg.length; j++){
-        var weatherId = data.result.data.weather[j].info.day[0];
+        var weatherId = data.result[j+1].weatid;
         chooseImg(weatherId,aImg[j]);
     }
 }
@@ -71,29 +81,50 @@ function changeImg(data){
 //选择图片
 function chooseImg(id,index){
     switch(id){
-        case '0':
+        case '1':
             index.src='images/weather_icon/1.png';
             break;
-        case '1':
+        case '2':
             index.src='images/weather_icon/2.png';
             break;
-        case '2':
+        case '3':
             index.src='images/weather_icon/3.png';
             break;
-        case '3':
-        case '7':
+        case '4':
+        case '5':
+        case '6':
         case '8':
+        case '9':
+        case '10':
+        case '11':
+        case '12':
+        case '13':
+        case '20':
+        case '22':
+        case '23':
+        case '24':
+        case '25':
+        case '26':
             index.src='images/weather_icon/4.png';
             break;
-        case '6':
+        case '7':
             index.src='images/weather_icon/6.png';
             break;
-        case '13':
         case '14':
         case '15':
         case '16':
+        case '17':
+        case '18':
+        case '27':
+        case '28':
+        case '29':
             index.src='images/weather_icon/5.png';
             break;
+        case '19':
+        case '21':
+        case '30':
+        case '31':
+        case '32':
         case '33':
             index.src='images/weather_icon/7.png';
             break;
@@ -110,6 +141,9 @@ function createUrl(){
     }else{
         cityName = arguments[0];
     }
-    var url = 'https://op.juhe.cn/onebox/weather/query?cityname=' + encodeURI(cityName) + '&key=1053d001421b886dcecf81a38422a1a2&callback=getWeather';
-    return url;
+    // console.log(cityName);
+    var urls = []; 
+    urls[0] = 'https://sapi.k780.com/?app=weather.future&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json&jsoncallback=getWeather&weaid=' + encodeURI(cityName);
+    urls[1] = 'https://api.map.baidu.com/telematics/v3/weather?output=json&ak=FK9mkfdQsloEngodbFl4FeY3&callback=getTodayWeather&location=' + encodeURI(cityName);
+    return urls;
 }
